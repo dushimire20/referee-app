90bb42e47e103a74fb8abe99702f12081624cf43
lionel <ndabagajeanlionel@gmail.com>
2024-09-15 20:53:45 +0200

    Refactor routing in App component to use Layout and DashboardLayout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "@/scenes/home";
 import SignUp from "@/scenes/signUp";
-import SignIn from "./scenes/signIn";
-import OurGoals from "./scenes/ourGoals";
-import KeyFeatures from "./scenes/keyFeatures";
-import Dashboard from "./scenes/dashboard";
-import ForgotPassword from "./scenes/forgotPassword";
-import Layout from "./components/Layout";
-import DashboardLayout from "./components/DashboardLayout";
+import SignIn from "@/scenes/signIn";
+import OurGoals from "@/scenes/ourGoals";
+import KeyFeatures from "@/scenes/keyFeatures";
+import DashboardHome from "@/scenes/dashboard";
+import ForgotPassword from "@/scenes/forgotPassword";
+import Layout from "@/components/Layout";
+import DashboardLayout from "@/components/DashboardLayout";
 
-
-function App() {
+const App = () => {
 	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route index element={<Home />} />
-						<Route path="/signUp" element={<SignUp />} />
-						<Route path="/login" element={<SignIn />} />
-						<Route path="/forgotPassword" element={<ForgotPassword />} />
-						<Route path="/goals" element={<OurGoals />} />
-						<Route path="/features" element={<KeyFeatures />} />
-					</Route>
-					<Route path="/dashboard/" element={<DashboardLayout />} >
-						<Route index element={<Dashboard />} />
-					</Route>
-				</Routes>
-			</Router>
-		</>
-	)
-}
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="signUp" element={<SignUp />} />
+					<Route path="login" element={<SignIn />} />
+					<Route path="forgotPassword" element={<ForgotPassword />} />
+					<Route path="goals" element={<OurGoals />} />
+					<Route path="features" element={<KeyFeatures />} />
+				</Route>
+
+				<Route path="dashboard" element={<DashboardLayout />}>
+					<Route index element={<DashboardHome />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
