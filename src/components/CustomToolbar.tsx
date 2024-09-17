c66e6f065fda130cd5962062f939e3e8b85dfecc
lionel <ndabagajeanlionel@gmail.com>
2024-09-17 21:32:32 +0200

    Unfinished: Update dependencies and styles for the schedule page

---
+++ src/components/CustomToolbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ToolbarProps, Event } from 'react-big-calendar';
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+interface ScheduleEvent extends Event {
+    title: string;
+    start: Date;
+    end: Date;
+    location: string;
+}
+
+const CustomToolbar: React.FC<ToolbarProps<ScheduleEvent, object>> = ({ date, onNavigate }) => {
+    const start = new Date(date);
+    const end = new Date(date);
+    start.setDate(start.getDate() - start.getDay());
+    end.setDate(end.getDate() - end.getDay() + 6);
+
+    const formatDate = (date: Date) => {
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = date.toLocaleString('default', { month: 'long' });
+        return { day, month };
+    };
+
+    const startDate = formatDate(start);
+    const endDate = formatDate(end);
+
+    return (
+        <div className="flex items-center mb-4 space-x-4 text-gray-500">
+            <span className="text-lg">
+                {startDate.month === endDate.month
+                    ? `${startDate.day} - ${endDate.day} ${startDate.month}`
+                    : `${startDate.day} ${startDate.month} - ${endDate.day} ${endDate.month}`}
+            </span>
+            <button onClick={() => onNavigate('PREV')} className="p-2 bg-secondary-100 bg-opacity-5 rounded-lg">
+                <MdOutlineKeyboardArrowLeft size={30} />
+            </button>
+            <button onClick={() => onNavigate('NEXT')} className="p-2 bg-secondary-100 bg-opacity-5 rounded-lg">
+                <MdOutlineKeyboardArrowRight size={30} />
+            </button>
+        </div>
+    );
+};
+
+export default CustomToolbar;
