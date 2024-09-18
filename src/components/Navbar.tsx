2ede007f4fac8f0268e26fd14dc210f9e3b994b2
lionel <ndabagajeanlionel@gmail.com>
2024-09-18 15:14:15 +0200

    Refactor: Simplify Navbar component and add search functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,25 @@
 import { FaBell, FaQuestionCircle } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
 function Navbar() {
 	return (
-		<nav className="flex sticky top-0 w-full justify-between items-center bg-white border-b p-4 text-xs z-10">
+		<nav className="flex sticky top-0 w-full justify-between items-center bg-white border-b p-4 text-xs">
 			<div className="flex-1">
-				<input
-					type="text"
-					placeholder="Search..."
-					className="border px-2 py-1 rounded-lg w-full max-w-xs"
-				/>
+				<div className="relative">
+					<FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"/>
+					<input
+						type="text"
+						placeholder="Search..."
+						className="border bg-gray-50 px-2 py-1 pl-8 rounded-lg w-full max-w-xs focus:outline-none"
+					/>
+				</div>
 			</div>
 			<div className="flex items-center gap-4">
-				<FaQuestionCircle size={20} className="cursor-pointer" />
-				<FaBell size={20} className="cursor-pointer" />
+				<FaQuestionCircle size={20} className="cursor-pointer"/>
+				<FaBell size={20} className="cursor-pointer"/>
 				<div className="flex items-center gap-2">
-					<img
-						src="https://via.placeholder.com/40" // Replace with actual user image
-						alt="User avatar"
-						className="rounded-full w-10 h-10"
-					/>
+					<FaUser size={40} className="rounded-full w-9 h-9 py-2 bg-secondary-100 bg-opacity-5"/>
 					<div className="hidden sm:block">
 						<p className="font-semibold">Eric ISHIMWE</p>
 						<p className="text-gray-400">Referee</p>
