60385210f639ce68364aef15f5c9cb6734dc43b1
“Lionel-hub1” <ndabagajeanlionel@gmail.com>
2024-10-03 18:06:54 +0200

    Update: Modified fields and state in Game creation modal

--- src/components/CreateGameModal.tsx
+++ src/components/CreateGameModal.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {FaXmark} from "react-icons/fa6";
+import {Referee, referees} from "@/data/usersRelatedData.ts";
+import {tournaments} from "@/data/gameRelatedData.ts";
 
-const CreateGameModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
-    const tournaments = ["Tournament 1", "Tournament 2", "Tournament 3"];
+const CreateGameModal: React.FC<{ onClose: () => void }> = ({onClose}) => {
     const teams = ["Team A", "Team B", "Team C", "Team D"];
     const locations = ["Location 1", "Location 2", "Location 3"];
 
     const [formData, setFormData] = useState({
-        tournament: "",
-        team1: "",
-        team2: "",
-        location: "",
-        crewChief: "",
-        tableRef1: "",
-        tableRef2: "",
-        tableRef3: "",
-        umpireRef1: "",
-        umpireRef2: ""
+        TOURNAMENT: "",
+        TIME: "",
+        DATE: "",
+        TEAM_A: "",
+        TEAM_B: "",
+        CATEG: "",
+        COURT: "",
+        STATUS: "Upcoming",
+        REFEREE: "",
+        UMPIRE_I: "",
+        UMPIRE_II: "",
+        SHOT_CLOCK: "",
+        TIMER: "",
+        ASS_SCORER: "",
+        SCORER: "",
+        SCORES: "coming soon..."
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        setFormData({...formData, [e.target.name]: e.target.value});
     };
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -31,7 +38,8 @@ const CreateGameModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-sm">
-            <div className="relative bg-white text-xs p-8 rounded-3xl shadow-lg z-60 w-full max-w-lg max-h-[95vh] overflow-y-auto z-50">
+            <div
+                className="relative bg-white text-xs p-8 rounded-3xl shadow-lg z-60 w-full max-w-lg max-h-[95vh] overflow-y-auto z-50">
                 <button onClick={onClose}
                         className="h-8 w-8 absolute right-4 top-4 flex justify-center items-center bg-secondary-100 bg-opacity-50 text-white rounded-full">
                     <FaXmark/>
@@ -40,77 +48,142 @@ const CreateGameModal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
                     <h2 className="text-xl font-bold">Create new Game</h2>
                 </div>
                 <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                        <label className="block text-sm font-bold">Tournament</label>
-                        <select name="tournament" value={formData.tournament} onChange={handleChange}
-                                className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
-                            <option value="">Select a tournament</option>
-                            {tournaments.map((tournament, index) => (
-                                <option key={index} value={tournament}>{tournament}</option>
-                            ))}
-                        </select>
+                    <label className="block text-sm font-bold">Tournament</label>
+                    <select name="TOURNAMENT" value={formData.TOURNAMENT} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                        <option value="">Select tournament</option>
+                        {tournaments.map((referee, index) => (
+                            <option key={index}
+                                    value={referee.id}>{referee.name}</option>
+                        ))}
+                    </select>
+                    <div className="flex space-x-4">
+                        <div className="w-full">
+                            <label className="block text-sm font-bold">Time</label>
+                            <input type="time" name="TIME" value={formData.TIME} onChange={handleChange}
+                                   className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        </div>
+                        <div className="w-full">
+                            <label className="block text-sm font-bold">Date</label>
+                            <input type="date" name="DATE" value={formData.DATE} onChange={handleChange}
+                                   className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        </div>
                     </div>
                     <div>
                         <label className="block text-sm font-bold">Teams</label>
                         <div className="flex space-x-2 items-center">
-                            <select name="team1" value={formData.team1} onChange={handleChange}
+                            <select name="TEAM_A" value={formData.TEAM_A} onChange={handleChange}
                                     className="w-1/2 mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
-                                <option value="">Select team 1</option>
+                                <option value="">Select team A</option>
                                 {teams.map((team, index) => (
                                     <option key={index} value={team}>{team}</option>
                                 ))}
                             </select>
                             <span>VS</span>
-                            <select name="team2" value={formData.team2} onChange={handleChange}
+                            <select name="TEAM_B" value={formData.TEAM_B} onChange={handleChange}
                                     className="w-1/2 mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
-                                <option value="">Select team 2</option>
+                                <option value="">Select team B</option>
                                 {teams.map((team, index) => (
                                     <option key={index} value={team}>{team}</option>
                                 ))}
                             </select>
                         </div>
                     </div>
+                    <div className="flex space-x-4">
+                        <div className="w-full">
+                            <label className="block text-sm font-bold">Category</label>
+                            <input type="text" name="CATEG" value={formData.CATEG} onChange={handleChange}
+                                   className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        </div>
+                        <div className="w-full">
+                            <label className="block text-sm font-bold">Court</label>
+                            <select name="COURT" value={formData.COURT} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select a court</option>
+                                {locations.map((location, index) => (
+                                    <option key={index} value={location}>{location}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+
+                    <label className="block text-sm font-bold">Arbitrators</label>
                     <div>
-                        <label className="block text-sm font-bold">Location</label>
-                        <select name="location" value={formData.location} onChange={handleChange}
+                        <select name="REFEREE" value={formData.REFEREE} onChange={handleChange}
                                 className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
-                            <option value="">Select a location</option>
-                            {locations.map((location, index) => (
-                                <option key={index} value={location}>{location}</option>
+                            <option value="">Select a referee</option>
+                            {referees.map((referee: Referee, index: number) => (
+                                <option key={index}
+                                        value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
                             ))}
                         </select>
                     </div>
-                    <label className="block text-sm font-bold">Referees</label>
                     <div className="flex space-x-4">
-
-                        <input type="text" placeholder="Crew Chief" name="crewChief" value={formData.crewChief}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
-                        <input type="text" placeholder="1st Table Ref" name="tableRef1" value={formData.tableRef1}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        <div className="w-full">
+                            <select name="UMPIRE_I" value={formData.UMPIRE_I} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select 1st umpire</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="w-full">
+                            <select name="UMPIRE_II" value={formData.UMPIRE_II} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select 2nd umpire</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-
                     <div className="flex space-x-4">
-                        <input type="text" placeholder="2nd Table Ref" name="tableRef2" value={formData.tableRef2}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
-
-                        <input type="text" placeholder="3rd Table Ref" name="tableRef3" value={formData.tableRef3}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        <div className="w-full">
+                            <select name="SHOT_CLOCK" value={formData.SHOT_CLOCK} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select Shot Clock</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="w-full">
+                            <select name="TIMER" value={formData.TIMER} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select Timer</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-
                     <div className="flex space-x-4">
-                        <input type="text" placeholder="1st Umpire Ref" name="umpireRef1" value={formData.umpireRef1}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
-
-                        <input type="text" placeholder="2nd Umpire Ref" name="umpireRef2" value={formData.umpireRef2}
-                               onChange={handleChange}
-                               className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none"/>
+                        <div className="w-full">
+                            <select name="ASS_SCORER" value={formData.ASS_SCORER} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select Assistant Scorer</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="w-full">
+                            <select name="SCORER" value={formData.SCORER} onChange={handleChange}
+                                    className="w-full mt-1 px-2 py-1 border rounded-2xl bg-gray-50 focus:outline-none">
+                                <option value="">Select Scorer</option>
+                                {referees.map((referee, index) => (
+                                    <option key={index}
+                                            value={referee.arbitratorId}>{referee.firstName} {referee.lastName}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
-
                     <div className="flex justify-center">
                         <button type="submit"
                                 className="w-full bg-secondary-100 text-white px-6 py-2 text-sm rounded-xl hover:bg-opacity-70">
