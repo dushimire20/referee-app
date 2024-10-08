a28f9e5648a1a07ceab2bb9180e38e5a03b535d2
dushimire20 <odushimire922@daviscollege.com>
2024-10-08 22:49:38 +0200

    my changes

--- src/scenes/GameManagement.tsx
+++ src/scenes/GameManagement.tsx
@@ -3,15 +3,14 @@ import {FaEdit, FaTrash, FaExternalLinkAlt} from "react-icons/fa";
 import CreateGameModal from "@/components/CreateGameModal.tsx";
 import {allGames, Game, tournaments} from "@/data/gameRelatedData.ts";
 import CreateTournamentModal from "@/components/CreateTournamentModal.tsx";
+import axios from "axios";
+import { Referee } from "@/data/usersRelatedData";
 
 
 
 const GameManagement: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isTournamentModalOpen, setIsTournamentModalOpen] = useState(false);
-    const [isRefereesModalOpen, setIsRefereesModalOpen] = useState(false);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [selectedGame, setSelectedGame] = useState<Game | null>(null);
+    const [isTournamentModalOpen, setIsTournamentModalOpen] = useState(false)
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -27,23 +26,35 @@ const GameManagement: React.FC = () => {
         setIsTournamentModalOpen(false);
     };
 
+    // backend
+    const [games, setGames] = useState<Game[]>([]);
+    const [referees, setReferees] = useState<Referee[]>([]);
+    const [teams, setTeams] = useState<Team[]>([]);
+    const [tournaments, setTournaments] = useState<Tournament[]>([]);
+
+    // Fetch upcomming games from the backend
+
+    useEffect( () => {
+
+        const fetchDashboardData = async () => {
+            try {
+              const response = await axios.get('http://localhost:3000/adminDashboard/dashboard');
+             console.log('Fetched games:', response.data.games); // Log the games to check structure
+              setGames(response.data.games);
+              setReferees(response.data.referees);
+              setTeams(response.data.teams);
+              setTournaments(response.data.tournaments);
+            } catch (error) {
+              console.error('Error fetching dashboard data:', error);
+            }
+          };
+
+        fetchDashboardData();       
+    }, []); // Empty depandacy array ensures this runs only when the component mounts
+
     return (
         <div
             className="container mx-auto p-8 flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 md:divide-x">
-            {isRefereesModalOpen && selectedGame && (
-                <EventDetailsModal game={selectedGame} onClose={closeRefereesModal}/>
-            )}
-            {isEditModalOpen && selectedGame && (
-                <EditGameModal
-                    game={selectedGame}
-                    onClose={closeEditModal}
-                    onSave={(updatedGame) => {
-                        // Here you would update the game in your data source
-                        console.log("Updated game:", updatedGame);
-                        closeEditModal();
-                    }}
-                />
-            )}
             <div className="w-full md:w-2/3">
                 {/* Game Management Header */}
                 <div className="flex flex-col md:flex-row justify-between items-center mb-4">
@@ -53,11 +64,11 @@ const GameManagement: React.FC = () => {
                         Add a game +
                     </button>
                 </div>
-                {isModalOpen && <CreateGameModal onClose={closeModal}/>}
+                {isModalOpen && <CreateGameModal onClose={closeModal} />}
 
                 {/* Game Management Cards */}
                 <div className="grid grid-cols-1 gap-4">
-                    {allGames.map((game, index) => (
+                    {games.map((game, index) => (
                         <div key={index} className="bg-secondary-100 bg-opacity-5 p-4 rounded-lg shadow-md text-xs">
                             <p className="mb-6">
                                 <span className="text-secondary-100">Game ID:</span> {game.id}
@@ -68,10 +79,14 @@ const GameManagement: React.FC = () => {
                                     className="flex flex-col sm:flex-row justify-around items-center w-full sm:w-11/12 space-y-4 sm:space-y-0">
                                     <div className="w-full sm:w-1/2 space-y-4">
                                         <p>
-                                            <span className="text-secondary-100">Teams:</span> {game.TEAM_A} vs {game.TEAM_B}
+                                            <span className="text-secondary-100">Teams: </span> 
+                                            {game.teams.map(teamId => {
+                                        const team = teams.find(t => t._id === teamId);
+                                        return team ? team.name : 'Unknown Team'; // Fallback if team not found
+                                        }).join(' VS ')}
                                         </p>
                                         <p>
-                                            <span className="text-secondary-100">Date and Time:</span> {game.DATE}
+                                            <span className="text-secondary-100">Date and Time:</span> {new Date(game.date).toLocaleDateString()} {game.time} 
                                         </p>
                                         <p>
                                             <span className="text-secondary-100">Location:</span> {game.court}
@@ -84,16 +99,14 @@ const GameManagement: React.FC = () => {
                                         <p>
                                             <span className="text-secondary-100">Score:</span> {game.SCORES}
                                         </p>
-                                        <p onClick={() => openRefereesModal(game)}
-                                           className="flex items-center cursor-pointer text-secondary-100 space-x-2">
+                                        <p className="flex items-center cursor-pointer text-secondary-100 space-x-2">
                                             <span>Referees in Charge</span>
                                             <FaExternalLinkAlt/>
                                         </p>
                                     </div>
                                 </div>
                                 <div className="flex space-x-4 w-full sm:w-1/12 mt-4 sm:mt-0">
-                                    <button onClick={() => openEditModal(game)}
-                                            className="text-green-500 hover:text-green-700">
+                                    <button className="text-green-500 hover:text-green-700">
                                         <FaEdit/>
                                     </button>
                                     <button className="text-red-500 hover:text-red-700">
@@ -105,7 +118,6 @@ const GameManagement: React.FC = () => {
                     ))}
                 </div>
             </div>
-
 
             {/* Active Tournaments */}
             <div className="w-full md:w-1/3 mt-8 md:mt-0 pl-0 md:pl-8">
@@ -138,12 +150,11 @@ const GameManagement: React.FC = () => {
                     ))}
                 </div>
                 <div className="mt-4 flex justify-end">
-                    <button onClick={openTournamentModal}
-                            className="bg-secondary-100 text-white px-6 py-3 text-sm rounded-xl hover:bg-opacity-70">
+                    <button onClick={openTournamentModal} className="bg-secondary-100 text-white px-6 py-3 text-sm rounded-xl hover:bg-opacity-70">
                         Create new tournament +
                     </button>
                 </div>
-                {isTournamentModalOpen && <CreateTournamentModal onClose={closeTournamentModal}/>}
+                {isTournamentModalOpen && <CreateTournamentModal onClose={closeTournamentModal} />}
             </div>
         </div>
     );
